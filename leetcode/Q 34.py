935fae1c451ae2caec59df6e276a04757a692618
Ritwik Gupta <ritwikg.0501@gmail.com>
2025-03-07 20:19:08 +0530

    Updates solution to Q34

--- leetcode/Q 34.py
+++ leetcode/Q 34.py
@@ -56,3 +56,39 @@ class Solution:
             j += 1
 
         return [i+1, j-1]
+    
+
+# Third Attempt
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        if not nums:
+            return [-1, -1]
+        
+        low = 0
+        high = len(nums)-1
+        i = -1
+
+        while low <= high:
+            mid = low + (high-low)//2
+            if nums[mid] == target:
+                i = mid
+                high = mid -1            
+            elif nums[mid] > target:
+                high = mid-1
+            else:
+                low = mid+1
+
+        low = 0
+        high = len(nums) - 1
+        j = -1
+        while low <= high:
+            mid = low + (high-low)//2
+            if nums[mid] == target:
+                j = mid
+                low = mid + 1
+            elif nums[mid] > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return [i, j]
